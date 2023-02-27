1bfa56e432aceb9722d576e3419acd1e090a386b
olatoCode <awosolat@gmail.com>
2023-02-27 03:21:29 +0100

    chores: cors and helmet added to package,json

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
+const helmet = require('helmet');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
 
+// base url for Home routes
+app.get('/', (req, res) => {
+  res.send({
+    Home: 'Welcome to Store Apis',
+  });
+});
+
+// connect to Database
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log('DB connection Successful'))
@@ -17,12 +27,17 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// routes for endpoints
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
 
+// connect to the server
 app.listen(process.env.PORT | 5000, () => {
   console.log('Backend sever is running');
 });
